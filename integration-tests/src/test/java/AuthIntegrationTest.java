6c3c3d0588e33fc93f6ac560532142f9f83dc1e8
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-10-17 14:26:30 -0400

    Tested Valid token and unauthorized token with junit

--- integration-tests/src/test/java/AuthIntegrationTest.java
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -1,2 +1,62 @@
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
 public class AuthIntegrationTest {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://localhost:4004"; //api-gateway address
+    }
+    //we should get a valid token with OK status code
+    @Test
+    public void shouldReturnOKWithValidToken() {
+        // 1. arrange
+        // 2. act
+        // 3. assert
+        String loginPayload = """
+          {
+            "email": "testuser@test.com",
+            "password": "password123"
+          }
+        """;
+
+        Response response = given() // 1. arrange
+                .contentType("application/json")
+                .body(loginPayload)
+                .when() // 2. act
+                .post("/auth/login")
+                .then() // 3. assert
+                .statusCode(200)
+                .body("token", notNullValue())
+                .extract()
+                .response();
+        System.out.println("Generated Token: " + response.jsonPath().getString("token") );
+    }
+
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidLogin() {
+        // 1. arrange
+        // 2. act
+        // 3. assert
+        String loginPayload = """
+          {
+            "email": "invalid_user@test.com",
+            "password": "wrongpassword"
+          }
+        """;
+
+         given() // 1. arrange
+                .contentType("application/json")
+                .body(loginPayload)
+                .when() // 2. act
+                .post("/auth/login")
+                .then() // 3. assert
+                .statusCode(401);
+
+    }
 }
